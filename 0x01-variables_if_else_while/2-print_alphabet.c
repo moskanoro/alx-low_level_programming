03a28360b332c690d7f9a78bfbf29c7999872882
moskanoro <mostapha.boudad.02@gmail.com>
2023-03-20 15:41:05 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+/**
+   * main - entry point
+    * Return: always 0 (success)
+     */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
