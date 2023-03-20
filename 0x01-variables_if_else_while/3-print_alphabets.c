4b9e8d2c8c102247067cd5fa88f164f01c2d7feb
moskanoro <mostapha.boudad.02@gmail.com>
2023-03-20 15:42:56 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+   * main - entry point
+    * Return: always 0 (success)
+     */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+	for (m = 65; m <= 90; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
