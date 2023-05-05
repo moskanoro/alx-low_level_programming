914d0c1d407fcb2af439097b4cc063c24be3494f
moskanoro <mostapha.boudad.02@gamil.com>
2023-05-04 19:20:59 -0700

    3. 11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,12 +5,12 @@
  * @n: pointer to the number to change
  * @index: index of the bit to set to 1
  *
- * Return: 1 for success, 0 for failure
+ * Return: 1 for success, -1 for failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
-		return (0);
+		return (-1);
 
 	*n = ((1UL << index) | *n);
 	return (1);
